46d27a850271cb3d5e8ae2bed8d15f8ed59151eb
dial2vishal <dial2vishal@gmail.com>
2017-10-16 07:16:46 -0400

    mylikes.php to manage like/unlike for any type

--- getrecipedetails.php
+++ getrecipedetails.php
@@ -5,11 +5,13 @@ include 'application_context.php';
 $filename = "getrecipedetails.php";
 
 $rcp_id = isset($_POST['rcp_id']) ? $_POST['rcp_id'] : '';
+$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
 
-$rcpdetailsquery = "SELECT RCP.RCP_ID, RCP.RCP_NAME, RCP.RCP_PROC, RCP.RCP_PLATING, RCP.RCP_NOTE, FDCSN.FOOD_CSN_NAME, FDTYP.FOOD_TYP_NAME,
+$rcpdetailsquery = "SELECT USR.NAME, RCP.RCP_ID, RCP.RCP_NAME, RCP.RCP_PROC, RCP.RCP_PLATING, RCP.RCP_NOTE, FDCSN.FOOD_CSN_NAME, FDTYP.FOOD_TYP_NAME,
                     FDCSN.FOOD_CSN_ID, FDTYP.FOOD_TYP_ID
                     from `RECIPE` AS RCP INNER JOIN `FOOD_CUISINE` AS FDCSN ON RCP.FOOD_CSN_ID = FDCSN.FOOD_CSN_ID
                                          INNER JOIN `FOOD_TYPE` AS FDTYP ON RCP.FOOD_TYP_ID = FDTYP.FOOD_TYP_ID
+																				 INNER JOIN `USER` AS USR ON RCP.USER_ID = USR.USER_ID 
                                          WHERE RCP.RCP_ID = '$rcp_id'";
 
  
@@ -22,8 +24,40 @@ $ingquery = "SELECT ING.ING_ID, ING.ING_NAME, QTY.QTY_NAME, DSH.ING_QTY FROM `DI
                                                             WHERE RCP.RCP_ID = '$rcp_id' ";
 
 
+$likequery = "SELECT COUNT(*) AS LIKES_COUNT FROM `LIKES` WHERE TYPE = 'RECIPE' AND TYPE_ID = '$rcp_id'";
+$userhaslikedquery = "SELECT COUNT(*) AS LIKES_COUNT FROM `LIKES` WHERE TYPE = 'RECIPE' AND TYPE_ID = '$rcp_id' AND USER_ID = '$user_id'";
 
+$checkviewquery = "SELECT COUNT(*) AS VIEWS_COUNT FROM `VIEWS` WHERE RCP_ID = '$rcp_id' AND USER_ID = '$user_id'";
+$insertviewquery = "INSERT INTO `VIEWS`(`USER_ID`, `RCP_ID`, `MOD_DTM`, `CREATE_DTM`) VALUES('$user_id', '$rcp_id', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+$viewquery = "SELECT COUNT(*) AS VIEWS_COUNT FROM `VIEWS` WHERE RCP_ID = '$rcp_id'";
 
+//if the view is not registered for this recipe by this user, register it
+try
+{
+  $check_view_data = mysqli_query($db,$checkviewquery);
+	infologger($filename, "I", "Recipe view data fetched successfully");
+	
+	if($viewobj = $check_view_data->fetch_object()) 
+  {
+      if($viewobj->VIEWS_COUNT == 0){
+					$q1_ok = "true";
+					$mysqli->query($insertviewquery) ? null : $q1_ok=false;
+
+					if(!$q1_ok)
+					{
+						 errlogger($filename, "E", "Query failure : ".$insertviewquery);
+					}
+					else{
+						infologger($filename, "I", "Recipe View data fetched successfully");
+					}
+			}
+	}
+}
+catch(Exception $e)
+{
+	  errlogger($filename, "E", 'Message: ' .$e->getMessage());
+}
+//if the view is not registered for this recipe by this user, register it
 
 try
 {
@@ -59,6 +93,38 @@ catch(Exception $e)
 	  errlogger($filename, "E", 'Message: ' .$e->getMessage());
 }
 
+
+try
+{
+	$like_data = mysqli_query($db,$likequery);
+	infologger($filename, "I", "Like data fetched successfully");
+}
+catch(Exception $e)
+{
+	  errlogger($filename, "E", 'Message: ' .$e->getMessage());
+}
+
+try
+{
+	$has_liked_data = mysqli_query($db,$userhaslikedquery);
+	infologger($filename, "I", "User like data fetched successfully");
+}
+catch(Exception $e)
+{
+	  errlogger($filename, "E", 'Message: ' .$e->getMessage());
+}
+
+try
+{
+	$view_data = mysqli_query($db,$viewquery);
+	infologger($filename, "I", "View data fetched successfully");
+}
+catch(Exception $e)
+{
+	  errlogger($filename, "E", 'Message: ' .$e->getMessage());
+}
+
+
 $rcpdetails = array();
 
  while($rcpobj = $rcp_data->fetch_object()) 
@@ -72,7 +138,20 @@ $rcpdetails = array();
       $rcpdetails['FOOD_TYP_NAME'] = $rcpobj->FOOD_TYP_NAME;
       $rcpdetails['FOOD_CSN_ID'] = $rcpobj->FOOD_CSN_ID;
       $rcpdetails['FOOD_TYP_ID'] = $rcpobj->FOOD_TYP_ID;
-  }
+	 		$rcpdetails['NAME'] = $rcpobj->NAME;
+	}
+
+if($likeobj = $like_data->fetch_object()){
+	$rcpdetails['likes'] = $likeobj->LIKES_COUNT;
+}
+
+if($userlikedobj = $has_liked_data->fetch_object()){
+	$rcpdetails['isLiked'] = $userlikedobj->LIKES_COUNT > 0;
+}
+
+if($viewobj = $view_data->fetch_object()){
+	$rcpdetails['views'] = $viewobj->VIEWS_COUNT;
+}
 
 $ingarray = array();
  while($ingobj = $ing_data->fetch_object()) 
---
+++ mylikes.php
@@ -0,0 +1,101 @@
+<?php
+  include 'application_context.php';
+
+  $filename = "mylikes.php";
+
+  //request
+  $user_id = isset($_POST['user_id']);
+  $type = isset($_POST['type']);
+  $type_id = isset($_POST['type_id']);
+  //request
+
+  try{
+    //check if $user_id has already liked $type & $type_id
+    $query = "SELECT LIKE_ID, IS_DEL FROM LIKES WHERE USER_ID = '$user_id' AND TYPE = '$type' AND TYPE_ID = '$type_id' ";
+    $result = mysqli_query($db,$query);
+
+    //if there is already an entry in LIKES table
+    if($result_data = $result->fetch_row()){
+      //if the user has unliked it, like it
+      if('Y' == $result_data->IS_DEL){
+        infologger($filename, "I", "The user(".$user_id.") has not liked the type(".$type.") with type id(".$type_id."). liking it.");
+
+        $query = "UPDATE LIKES SET IS_DEL = 'N' WHERE USER_ID = '$user_id' AND TYPE = '$type' AND TYPE_ID = '$type_id' ";
+
+        if(mysqli_query($db,$query)){
+          infologger($filename, "I", "The user(".$user_id.") has liked the type(".$type.") with type id(".$type_id.") successfully.");
+        }
+        else{
+          errlogger($filename, "E", "Failed !! The user(".$user_id.") could not like the type(".$type.") with type id(".$type_id.") successfully.");
+        }
+      }
+      //if the user has unliked it, like it
+      //if the user has liked it, unlike it
+      else{
+        infologger($filename, "I", "The user(".$user_id.") has already liked the type(".$type.") with type id(".$type_id."). unliking it.");
+
+        $query = "UPDATE LIKES SET IS_DEL = 'Y' WHERE USER_ID = '$user_id' AND TYPE = '$type' AND TYPE_ID = '$type_id' ";
+
+        if(mysqli_query($db,$query)){
+          infologger($filename, "I", "The user(".$user_id.") has unliked the type(".$type.") with type id(".$type_id.") successfully.");
+        }
+        else{
+          errlogger($filename, "E", "Failed !! The user(".$user_id.") could not unlike the type(".$type.") with type id(".$type_id.") successfully.");
+        }
+      }
+      //if the user has liked it, unlike it
+    }
+    //if there is already an entry in LIKES table
+    //if there is no entry in LIKES table
+    else{
+      infologger($filename, "I", "The user(".$user_id.") has not yet liked the type(".$type.") with type id(".$type_id."). So liking it");
+
+      $query = "INSERT INTO `LIKES` (`USER_ID`, `TYPE`, `TYPE_ID` , `CREATE_DTM`, `MOD_DTM`) VALUES ('$user_id', '$type', '$type_id',  CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+
+      if($mysqli->query($likesql)){
+        infologger($filename, "I" , "The user(".$user_id.") has liked the type(".$type.") with type id(".$type_id.") successfully.");
+      }
+      else{
+        errlogger($filename, "E", "Failed !! The user(".$user_id.") could not like the type(".$type.") with type id(".$type_id.") successfully.");
+      } 
+    }
+    //if there is no entry in LIKES table
+    //check if $user_id has already liked $type & $type_id
+
+    $result_array = array();
+
+    //check the status (liked/unliked)
+    $query = "SELECT COUNT(*) AS LIKES_COUNT FROM LIKES WHERE USER_ID = '$user_id' AND TYPE = '$type' AND TYPE_ID = '$type_id' AND IS_DEL = 'N'";
+    $result = mysqli_query($db,$query);
+
+    if($result_data = $result->fetch_row()){
+      if($result_data->LIKES_COUNT != 0){
+        $result_array['isliked'] = true;
+      }
+      else{
+        $result_array['isliked'] = false;
+      }
+
+      infologger($filename, "I" , "The user(".$user_id.") for the type(".$type.") with type id(".$type_id.") has liked ? ".$result_array['isliked']);
+    }
+    //check the status (liked/unliked)
+
+    //get total likes for the $type & $type_id
+    $query = "SELECT COUNT(*) AS LIKES_COUNT FROM LIKES WHERE TYPE = '$type' AND TYPE_ID = '$type_id' AND IS_DEL = 'N'";
+    $result = mysqli_query($db,$query);
+
+    if($result_data = $result->fetch_row()){
+      $result_array['likes'] = $result_data->LIKES_COUNT;
+
+      infologger($filename, "I" , "The type(".$type.") with type id(".$type_id.") has been liked ".$result_array['likes']." times");
+    }
+    //get total likes for the $type & $type_id
+
+    //response
+    echo json_encode($result_array);
+    //response
+  }
+  catch(Exception $e){
+    errlogger($filename, "E", 'Message: ' .$e->getMessage());
+  }
+?>
