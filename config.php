aad48490434a2d712ae78c52c975c6120f25b3d6
Ajit Kamath <ajitkamathk@gmail.com>
2017-11-05 10:55:14 +0530

    configure centralised database in 000webhost.com

--- config.php
+++ config.php
@@ -1,11 +1,9 @@
 <?php
-
 $filename = "config.php";
-
-$hostname = "sql12.freemysqlhosting.net";
-$user = "sql12202940";
-$password = "tiDHfUMZEq";
-$database = "sql12202940";
+$hostname = "deicidal-haul.000webhostapp.com";
+$user = "id3505680_cookery";
+$password = "cookeryuser";
+$database = "id3505680_cookery";
 $db = mysqli_connect($hostname, $user, $password);
 if (mysqli_connect_errno())
   {
@@ -13,7 +11,6 @@ if (mysqli_connect_errno())
 	errlogger($filename, "E", "Failed to connect to MySQL: " . mysqli_connect_error());
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-
 else{
 //			echo "Connected sucessfully";
 	}
